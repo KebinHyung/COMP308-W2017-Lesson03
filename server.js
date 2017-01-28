8dc1da3ef2928e6aa1897d63cdf321b9c7fa2299
Kevin Luu <kevin_luu_naruto@hotmail.com>
2017-01-28 13:42:34 -0500

    Conversion to Express

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// your server will live here
 
-// import the 3rd party connect module and
-// assign it as a reference to the connect variable
-let connect = require('connect');
+let express = require('express');
 
-// create an instance of the connect server - app
-let app = connect();
+// create an instance of the express server - app
+let app = express();
 
 // create the port constant
 const port = 3000;
